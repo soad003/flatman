0e508abbdb6618015602fcf14b9819d5b9d34ebf
Michae Froewis <michael.froewis@student.uibk.ac.at>
2014-03-09 14:22:23 +0100

    secret token git ignore issue

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flatman::Application.config.secret_key_base = '0a20189d4cdef6ce3e93ae4aba34f0c49058418f252229fcf78ca5329ad9fa1705fe13fc258c3a0649c61fe764290b7dfa7e1c01ec050cb11ea17db92b391106'
