4df3b2daccccfe638ea2a57a4af159701356b895
Michael Froewis <michael.froewis@student.uibk.ac.at>
2014-03-18 14:33:24 +0100

    delete unused migrations

--- db/migrate/20140313190336_create_shopping_lists.rb
+++
@@ -1,9 +0,0 @@
-class CreateShoppingLists < ActiveRecord::Migration
-  def change
-    create_table :shopping_lists do |t|
-      t.string :item_name
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20140314100721_create_test2s.rb
+++
@@ -1,9 +0,0 @@
-class CreateTest2s < ActiveRecord::Migration
-  def change
-    create_table :test2s do |t|
-      t.string :test
-
-      t.timestamps
-    end
-  end
-end
