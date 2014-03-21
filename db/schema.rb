e5d51c9f903112c6dc8d2c51eb2deb8ad9b6e923
raffi92 <raphael.gwiggner@gmail.com>
2014-03-21 10:13:39 +0100

    FinancesUIPrototype

--- db/schema.rb
+++ db/schema.rb
@@ -13,18 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140316141851) do
 
-  create_table "shopping_lists", force: true do |t|
-    t.string   "item_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "test2s", force: true do |t|
-    t.string   "test"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "users", force: true do |t|
     t.string   "provider"
     t.string   "uid"
