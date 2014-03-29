fbbc2dc6fa08a804f522ca62b8566b714641190e
Michael Froewis <michael.froewis@student.uibk.ac.at>
2014-03-29 15:20:26 +0100

    navbar active fix, switch local angular fix

--- app/assets/javascripts/controller/root.js
+++ app/assets/javascripts/controller/root.js
@@ -1,5 +1,9 @@
-angular.module('flatman').controller("rootCtrl",function($scope,Util){
+angular.module('flatman').controller("rootCtrl",function($scope,Util,$location){
     $scope.Util=Util;
+
+    $scope.isActive = function(route) {
+        return route === $location.path();
+    }
 
     $scope.search=function(){
         Util.redirect_to.search($scope.search_term);
