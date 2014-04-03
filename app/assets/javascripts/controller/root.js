17faae5a0fab2be189e07a1e8b1f4e8701d8b5f2
Michael Froewis <michael.froewis@student.uibk.ac.at>
2014-04-03 09:24:56 +0200

    error messages v2

--- app/assets/javascripts/controller/root.js
+++ app/assets/javascripts/controller/root.js
@@ -1,5 +1,6 @@
 angular.module('flatman').controller("rootCtrl",function($scope,$timeout,$location,Util,statusService){
     $scope.Util=Util;
+    $scope.error_type="danger";
 
     $scope.isActive = function(route) {
         return route === $location.path();
@@ -7,6 +8,10 @@ angular.module('flatman').controller("rootCtrl",function($scope,$timeout,$locati
 
     $scope.search=function(){
         Util.redirect_to.search($scope.search_term);
+    };
+
+    $scope.delete_error=function(index){
+        Util.delete_server_error(index);
     };
 
     (function tick() {
