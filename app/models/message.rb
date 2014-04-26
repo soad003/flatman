1c9b2857e90c2f4ed231d6b615fa780c8c9f7a30
schoepfi88 <ch.schoepf1@hotmail.com>
2014-04-26 23:24:07 +0200

    message sort chats

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,10 +2,24 @@ class Message < ActiveRecord::Base
 	belongs_to	:sender, :class_name => 'User'
 	belongs_to 	:receiver, :class_name => 'User'
 
-	def self.find_chat(id, user)
-        mesL = Message.where(sender_id: user.id)
-        mesL2 = Message.where(receiver_id: user.id)
+	def self.find_messages(user, user2)
+		mesL = Message.where(['sender_id = ? AND receiver_id = ?', user.id, user2])
+        mesL2 = Message.where(['receiver_id = ? AND sender_id = ?', user.id, user2])
+        #mesL = Message.where(sender_id: user.id)
+        #mesL2 = Message.where(receiver_id: user.id)
         mesL+mesL2
+    end
+
+    def self.find_chats(user)
+    	mesL = Message.where(sender_id: user.id)
+       	mesL2 = Message.where(receiver_id: user.id)
+        messList = mesL+mesL2
+        # sort by newest
+        messList.sort! { |a,b| b.created_at <=> a.created_at }
+        # remove already displayed chats
+        messList.each do |mes| 
+      		#
+    	end
     end
 
 end
