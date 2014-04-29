778e4f3df600a0fa196f483ea287c44c1cb918c2
Michael Froewis <michael.froewis@student.uibk.ac.at>
2014-04-29 16:45:11 +0200

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140422125526) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "billcategories", force: true do |t|
     t.integer  "flat_id"
     t.string   "name"
