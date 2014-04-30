ed87c2ba0487af5e28131184d92c668784c639cb
Clemens Brunner <cle1000.cb@gmail.com>
2014-04-30 17:49:49 +0200

    production: try to enable assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Flatman::Application.configure do
 
   # Compress JavaScripts and CSS.
   #config.assets.js_compressor = Uglifier.new(:mangle => false)
-  config.assets.enabled = false
+  #config.assets.enabled = false
   #config.assets.debug = true
   # config.assets.css_compressor = :sass
 
