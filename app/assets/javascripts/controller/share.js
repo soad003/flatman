22352ba4dc6d5e85a1d85e12d6317bfee8555ebb
Niko Risslegger <niko1003@gmx.net>
2014-05-02 23:46:39 +0200

    jshint failures

--- app/assets/javascripts/controller/share.js
+++ app/assets/javascripts/controller/share.js
@@ -1,49 +1,51 @@
-angular.module('flatman').controller("shareCtrl", function($scope, shareService, Util, $location){
-  	$scope.shareditems = shareService.items.get();  	
-	
+angular.module('flatman').controller("shareCtrl", function($scope, shareService, Util, $location) {
+	$scope.shareditems = shareService.items.get();
+
 	//add tooltips (data in title="") to classes.
-	$('.panel-tooltip').tooltip({placement: "bottom"});
-	
-	  	//add a new fucking item. just title.
-  	$scope.addItem = function() {
-  	 	shareService.items.create($scope.newItemName, function(data) {
-  	 		data.items = [];
-  	 		$scope.shareditems.push(data);
-  	 		$scope.newItemName = '';
-  	 		window.location = "#/shareditem/"+data.id;
-  	 	}, function() {
-  	 		//errorhandling
-  	 	});
-  	     
-  	 };
-	
+	$('.panel-tooltip').tooltip({
+		placement : "bottom"
+	});
+
+	//add a new fucking item. just title.
+	$scope.addItem = function() {
+		shareService.items.create($scope.newItemName, function(data) {
+			data.items = [];
+			$scope.shareditems.push(data);
+			$scope.newItemName = '';
+			window.location = "#/shareditem/" + data.id;
+		}, function() {
+			//errorhandling
+		});
+
+	};
+
 	$scope.removeItem = function(item) {
 		bootbox.confirm("Are you sure?", function(result) {
 			if (result) {
-			    shareService.items.remove(item, function(data) {
+				shareService.items.remove(item, function(data) {
 					$scope.shareditems = _($scope.shareditems).without(item);
 				}, function() {
-				});	
-		    } 		
+				});
+			}
 		});
-			
-	};  
-	
-	
+
+	};
+
 	$scope.hideItem = function(item, hide) {
 		item.hidden = hide;
 		shareService.items.update(item, function(data) {
-			
+
 		}, function() {
-			
+
 		});
 	};
-	
+
 	$scope.borrowItem = function(item) {
 		item.available = !item.available;
 		shareService.items.update(item, function(data) {
-			
-		}, function() {});
+
+		}, function() {
+		});
 	};
 
 });
--- app/assets/javascripts/services/share.js
+++ app/assets/javascripts/services/share.js
@@ -1,69 +1,81 @@
 angular.module('flatman').factory("shareService", function($resource) {
-	
-	var sharedItemService = $resource('/api/shareditem/:id', {
-		
-	}, {
-		'get': {method: "GET"},
-		'update': {method: "PUT"},
-		'destroy': {method: "DELETE"},
-		'upload': {method: "PUT"}
-	})
-	
-	var shareService = $resource('/api/share/:id', {
-		
-	}, {
-	    'get': {method: "GET", isArray:true},
-	    'create': {method: "POST"},
-	    'destroy': {method: "DELETE"}
+	var sharedItemService = $resource('/api/shareditem/:id', {}, {
+		'get' : {
+			method : "GET"
+		},
+		'update' : {
+			method : "PUT"
+		},
+		'destroy' : {
+			method : "DELETE"
+		},
+		'upload' : {
+			method : "PUT"
+		}
 	});
-	
+
+	var shareService = $resource('/api/share/:id', {
+
+	}, {
+		'get' : {
+			method : "GET",
+			isArray : true
+		},
+		'create' : {
+			method : "POST"
+		},
+		'destroy' : {
+			method : "DELETE"
+		}
+	});
 
 	return {
-		item: {
-			get:
-				function(itemid, succH, errH) { 
-					return sharedItemService.get({id:itemid}, {}, succH, errH); 
-				},
-			update: 
-				function(item, succH, errH) {
-					return sharedItemService.update({id:item.id}, item, succH, errH);
-		
-				},
-			upload: 
-				function(item, succH, errH) {
-					return sharedItemService.upload({id : item.id}, item, succH, errH);
-				},
-			
+		item : {
+			get : function(itemid, succH, errH) {
+				return sharedItemService.get({
+					id : itemid
+				}, {}, succH, errH);
+			},
+			update : function(item, succH, errH) {
+				return sharedItemService.update({
+					id : item.id
+				}, item, succH, errH);
+
+			},
+			upload : function(item, succH, errH) {
+				return sharedItemService.upload({
+					id : item.id
+				}, item, succH, errH);
+			}
 		},
-		
+
 		//overview
 		/*warum is die reihenfolge von die services relevanta?!?!??!?*/
-		items: {	
-			remove:
-				function(item, succH, errH) {
-					shareService.destroy({id : item.id}, succH, errH);
-					
-				},
-			update:
-				function(item, succH, errH) {
-					console.log(item);
-					sharedItemService.update({id:item.id}, item, succH, errH);
-				},
-			get: 
-				function(){ 
-					return shareService.get();
-				},
+		items : {
+			remove : function(item, succH, errH) {
+				shareService.destroy({
+					id : item.id
+				}, succH, errH);
 
-       		create: 
-       			function(name, succH, errH) {
-	        		console.log(name);
-	                shareService.create(null, {name: name}, succH, errH);
-            	},
 			},
-		
-	
+			update : function(item, succH, errH) {
+				console.log(item);
+				sharedItemService.update({
+					id : item.id
+				}, item, succH, errH);
+			},
+			get : function() {
+				return shareService.get();
+			},
+
+			create : function(name, succH, errH) {
+				console.log(name);
+				shareService.create(null, {
+					name : name
+				}, succH, errH);
+			}
 		}
-		
-		
-	
+
+	}
+
 });
