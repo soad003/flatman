56099b24e0a9e3f8c2de8f733fe9d2ef8dfb18fa
Michael Froewis <michael.froewis@student.uibk.ac.at>
2014-05-03 16:00:02 +0200

    rename shareditem Migration due to name confilict

--- db/migrate/20140422125526_shareditem.rb
+++ db/migrate/20140422125526_shareditem.rb
@@ -1,4 +1,4 @@
-class Shareditem < ActiveRecord::Migration
+class ChangeColumnsShareditem < ActiveRecord::Migration
   def change
     change_column :shareditems, :available, :boolean, :default => false
   end
