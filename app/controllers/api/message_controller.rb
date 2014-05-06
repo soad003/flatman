2f6f71a9468f1ddd5a833e73193e9888d0385dad
schoepfi88 <ch.schoepf1@hotmail.com>
2014-05-06 18:57:29 +0200

    message view update

--- app/controllers/api/message_controller.rb
+++ app/controllers/api/message_controller.rb
@@ -31,15 +31,10 @@ class Api::MessageController < Api::RestController
       @mes.header = "0"
       beginDate2014 = Time.new(2014,3,30,2,0)
       endDate2014 = Time.new(2014,10,26,3,0)
-      nowTime = Time.at(Time.now.to_i + 3600)
-      if nowTime.between?(beginDate2014, endDate2014)
-        @mes.created_at = Time.at(nowTime.to_i + 3600)
-      else 
-        @mes.created_at = nowTime
-      end
       beginDate2015 = Time.new(2015,3,29,2,0)
       endDate2015 = Time.new(2015,10,25,3,0)
-      if nowTime.between?(beginDate2015, endDate2015)
+      nowTime = Time.at(Time.now.to_i + 3600)
+      if nowTime.between?(beginDate2014, endDate2014) || nowTime.between?(beginDate2015, endDate2015)
         @mes.created_at = Time.at(nowTime.to_i + 3600)
       else 
         @mes.created_at = nowTime
