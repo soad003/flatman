974f7c856decb646beb026a87ce63ddd6a7db9a4
Clemens <cle1000.cb@gmail.com>
2016-04-04 11:37:51 +0200

    fix currencey bug

--- app/assets/javascripts/filter/customCurrency.js
+++ app/assets/javascripts/filter/customCurrency.js
@@ -3,7 +3,7 @@ var app = angular.module('flatman');
 app.filter('customCurrency', ["$filter", function ($filter) {
     return function(amount, currencySymbol){
         var currency = $filter('currency');
-		var res = currency(amount, currencySymbol);
+		var res = currency(amount, ''); //currencySymbol);
         if(res.indexOf("(") != -1){
              res = res.replace("(", "").replace(")", "").split(" ");
              return res[0] + " -" + res[1]; 
